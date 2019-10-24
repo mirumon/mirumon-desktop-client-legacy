04cef95d3df16c3712ad5ffc445e6ea8148fcb60
Slyfoxy <khitrov34@gmail.com>
2019-10-24 14:56:17 +0300

    replace bash scripts to `.bat` scripts

--- scripts/format.bat
+++ scripts/format.bat
@@ -1,10 +1,4 @@
-#!/usr/bin/env bash
-
-set -e
-
-# Sort imports one per line, so autoflake can remove unused imports
 isort --recursive  --force-single-line-imports app
-
 autoflake --recursive --remove-all-unused-imports --remove-unused-variables --in-place app  --exclude=__init__.py
 black app
 isort --recursive --multi-line=3 --trailing-comma --line-width 88 --force-grid-wrap=0 --combine-as app
--- scripts/lint.bat
+++ scripts/lint.bat
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
 black --check app
 isort --recursive app
+flake8 app
+mypy app
