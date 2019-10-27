a456f7f3d1b569a4ea869f18ca3182d3d94f8011
Slyfoxy <khitrov34@gmail.com>
2019-10-27 14:58:03 +0300

    remove isort params in format script

--- scripts/format.bat
+++ scripts/format.bat
@@ -1,4 +1,4 @@
 isort --recursive  --force-single-line-imports app
 autoflake --recursive --remove-all-unused-imports --remove-unused-variables --in-place app  --exclude=__init__.py
 black app
-isort --recursive --multi-line=3 --trailing-comma --line-width 88 --force-grid-wrap=0 --combine-as app
+isort --recursive  app
