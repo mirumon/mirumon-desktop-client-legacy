191c7c2edc04e80c9e3cdd5c1dc09bb0ac1422b2
Slyfoxy <khitrov34@gmail.com>
2020-04-06 02:29:27 +0300

    Fix venv path for windows service installing

--- win_service.py
+++ win_service.py
@@ -2,25 +2,66 @@
 Script for starting mirumon client like windows service
 
 Commands:
-python "P:\Python3\Scripts\pywin32_postinstall.py" -install
+python "venv\Scripts\pywin32_postinstall.py" -install
 python.exe .\win_service.py --startup=auto install
 python.exe .\win_service.py start
 NET START MirumonService
 
-Links:
-https://www.chrisumbel.com/article/windows_services_in_python
-https://www.thepythoncorner.com/2018/08/how-to-create-a-windows-service-in-python/
-
+http://thepythoncorner.com/dev/how-to-create-a-windows-service-in-python/
 """
+
+import os, sys, site
+
+project_name = "mirumon-desktop-client"  # Change this for your own project !!!!!!!!!!!!!!
+venv_folder_name = "venv"  # Change this for your own venv path !!!!!!!!!!!!!!
+
+if sys.executable.lower().endswith("pythonservice.exe"):  # https://issue.life/questions/34696815
+
+    # Get root path for the project
+    service_directory = os.path.abspath(os.path.dirname(__file__))
+    project_directory = service_directory[:service_directory.find(project_name)+len(project_name)]
+
+    # Get venv path for the project
+    def file_path(x): return os.path.join(project_directory, x)
+    venv_base = file_path(venv_folder_name)
+    venv_scripts = os.path.join(venv_base, "Scripts")
+    venv_packages = os.path.join(venv_base, 'Lib', 'site-packages')
+
+    # Change current working directory from PythonService.exe location to something better.
+    os.chdir(project_directory)
+    sys.path.append(".")
+    prev_sys_path = list(sys.path)
+
+    # Manually activate a virtual environment inside an already initialized interpreter.
+    os.environ['PATH'] = venv_scripts + os.pathsep + os.environ['PATH']
+
+    site.addsitedir(venv_packages)
+    sys.real_prefix = sys.prefix
+    sys.prefix = venv_base
+
+    # Move some sys path in front of others
+    new_sys_path = []
+    for item in list(sys.path):
+        if item not in prev_sys_path:
+            new_sys_path.append(item)
+            sys.path.remove(item)
+    sys.path[:0] = new_sys_path
+
+
 import asyncio
 import logging
 
 import servicemanager
 import win32event
 import win32service
+print(1)
+for k,v in os.environ.items():
+    print(k,v)
+
+print(os.getcwd())
+from loguru import logger
 import win32serviceutil
 import wmi
-from loguru import logger
 
 from app import config
 from app.main import Lifespan, server_connection_with_retry
