de94804d91cc857196890271e4f48b115ce835f4
Nik Sidnev <sidnev.nick@gmail.com>
2020-04-12 04:34:57 +0300

    introduce main hidden command to cli

--- mirumon/cli/main.py
+++ mirumon/cli/main.py
@@ -4,13 +4,31 @@ import typer
 import subprocess
 import platform
 
-from mirumon.cli.groups import core
-
+from mirumon.client.main import run_service
 
 cli = typer.Typer()
 
-cli.add_typer(core)
-
 if platform.system() == "Windows":
     from mirumon.cli.groups import windows
-    cli.add_typer(windows)
+
+    cli.add_typer(windows.group)
+
+
+
+@cli.command(hidden=True)
+def run(
+    token: str, server: str, reconnect_delay: int = 10, reconnect_attempts: int = 10
+) -> None:
+    create_logs_dir()
+
+    message = "\n".join(
+        [
+            "Start service with current config",
+            f"token: {token}",
+            f"server: {server}",
+            f"reconnect delay: {reconnect_delay}",
+            f"reconnect attempts: {reconnect_attempts}",
+        ]
+    )
+    typer.echo(message)
+    # run_service()
